813d9cd99efd569a92e890b68486045d6b065ac6
Rasyid1609 <rasyid.mahardian@gmail.com>
2025-09-05 17:10:12 +0700

    V.03

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // User dummy (opsional)
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User admin untuk login
+        User::updateOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('admin123'), // password default
+            ]
+        );
     }
 }
